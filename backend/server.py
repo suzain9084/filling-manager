b0a393f7d59161c4c44bf9288a98c307ea2c35e9
suzain9084 <suzain@ce.du.ac.in>
2025-06-29 22:18:53 +0530

    remove debug true

--- backend/server.py
+++ backend/server.py
@@ -558,5 +558,3 @@ def addBookMarks():
 def helloRoute():
     return jsonify({"Name":"suzain"})
 
-if __name__ == '__main__':
-    app.run(debug=False, use_reloader=False, host='0.0.0.0', port=int(port) if port else 5000)
